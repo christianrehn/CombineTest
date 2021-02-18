bd23c7fa8b702e6b0bcdd226f28d353ee0b0454b
rehn <rehn@itestra.de>
2021-02-18 19:29:23 +0100

    tray menu

--- src/electron/TrayMenu.ts
+++ src/electron/TrayMenu.ts
@@ -1,48 +1,49 @@
-import { app, Tray, Menu, nativeImage } from 'electron';
-import { appManager } from './AppManager';
+import {app, Tray, Menu, nativeImage} from 'electron';
+import {appManager} from './AppManager';
 
 export class TrayMenu {
-  // Create a variable to store our tray
-  // Public: Make it accessible outside of the class;
-  // Readonly: Value can't be changed
-  public readonly tray: Tray;
+    // Create a variable to store our tray
+    // Public: Make it accessible outside of the class;
+    // Readonly: Value can't be changed
+    public readonly tray: Tray;
 
-  // Path where should we fetch our icon;
-  private iconPath: string = '/assets/clock-icon.png';
+    // Path where should we fetch our icon;
+    private iconPath: string = '/assets/golf-icon.png';
 
 
-  constructor() {
-    this.tray = new Tray(this.createNativeImage());
-    this.tray.setContextMenu(this.createMenu());
-    // this.tray.setToolTip('Deskink')
-  }
+    constructor() {
+        this.tray = new Tray(this.createNativeImage());
+        this.tray.setContextMenu(this.createMenu());
+        // this.tray.setToolTip('Deskink')
+    }
 
-  createNativeImage() {
-    // Since we never know where the app is installed,
-    // we need to add the app base path to it.
-    const path = `${app.getAppPath()}${this.iconPath}`;
-    console.log(path);
-    const image = nativeImage.createFromPath(path);
-    // Marks the image as a template image.
-    image.setTemplateImage(true);
-    return image;
-  }
+    createNativeImage() {
+        // Since we never know where the app is installed,
+        // we need to add the app base path to it.
+        const path: string = `${app.getAppPath()}${this.iconPath}`;
+        console.log(path);
+        const image: Electron.NativeImage = nativeImage.createFromPath(path);
+        // Marks the image as a template image.
+        image.setTemplateImage(true);
+        return image;
+    }
 
-  createMenu(): Menu {
-    const contextMenu = Menu.buildFromTemplate([
-      {
-        label: 'Tokei',
-        type: 'normal',
-        click: () => { 
-          /* Later this will open the Main Window */ appManager.getWindow('AlarmWindow').window.show();
-        }
-      },
-      {
-        label: 'Quit',
-        type: 'normal',
-        click: () => app.quit()
-      }
-    ]);
-    return contextMenu;
-  }
+    createMenu(): Menu {
+        const contextMenu: Menu = Menu.buildFromTemplate([
+            {
+                label: 'Tokei',
+                type: 'normal',
+                click: () => {
+                    /* Later this will open the Main Window */
+                    appManager.getWindow('AlarmWindow').window.show();
+                }
+            },
+            {
+                label: 'Quit',
+                type: 'normal',
+                click: () => app.quit()
+            }
+        ]);
+        return contextMenu;
+    }
 }
