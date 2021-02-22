8fdf0f677f29951103fad4e958f9a714e1cefee7
rehn <rehn@itestra.de>
2021-02-22 18:30:59 +0100

    t

--- src/app/components/Toggle/Toggle.tsx
+++ src/app/components/Toggle/Toggle.tsx
@@ -2,21 +2,22 @@ import React from 'react';
 // import './Toggle.scss';
 
 type ToggleProps = {
-  checked?: boolean;
-  handler?: () => void;
+    checked?: boolean;
+    handler?: () => void;
 }
 
 const Toggle = ({
-  handler = () => {},
-}: ToggleProps) => {
-  return (
-    <label className="toggle">
-      <input type="checkbox"
-        onChange={handler}
-      />
-      <span className="slider"></span>
-    </label>
-  );
+                    handler = (): void => {
+                    },
+                }: ToggleProps) => {
+    return (
+        <label className="toggle">
+            <input type="checkbox"
+                   onChange={handler}
+            />
+            <span className="slider"></span>
+        </label>
+    );
 }
 
 export default Toggle;
