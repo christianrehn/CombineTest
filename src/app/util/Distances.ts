57d6c1c809463d0fd6614c613e4643e2fba27f95
rehn <rehn@itestra.de>
2021-02-24 18:02:19 +0100

    t

--- src/app/util/Distances.ts
+++ src/app/util/Distances.ts
@@ -1,5 +1,18 @@
 import {assert} from "chai";
 
+/**
+ * Create random number between [min, max[.
+ */
+const createRandomNumber = (min: number, max: number): number => {
+    assert(min <= max, "min > max");
+
+    if (min === max) {
+        return min;
+    }
+
+    return Math.floor(Math.random() * Math.floor(max - min)) + min;
+}
+
 export interface IDistances {
     getNext: (index: number) => number;
 }
@@ -8,17 +21,11 @@ export class RandomDistances implements IDistances {
     readonly minDistance: number = 10;
     readonly maxDistance: number = 60;
 
-    /**
-     * Create random number between [min, max[.
-     */
-    private createRandomNumber(min: number, max: number): number {
-        assert(min < max, "min >= max");
-
-        return Math.floor(Math.random() * Math.floor(max - min)) + min;
-    }
 
     public getNext(index: number): number {
-        return this.createRandomNumber(this.minDistance, this.maxDistance);
+        assert(index >= 0, "index < 0");
+
+        return createRandomNumber(this.minDistance, this.maxDistance);
     }
 }
 
@@ -26,7 +33,41 @@ export class FixedDistances implements IDistances {
     readonly distances: number[] = [10, 20, 30];
 
     public getNext(index: number): number {
-        console.log("getNext ", index)
+        assert(index >= 0, "index < 0");
+
         return this.distances[index % this.distances.length];
+    }
+}
+
+export class RandomFromFixedDistances implements IDistances {
+    readonly distances: number[] = [10, 20, 30, 40];
+    private distancesNotYetReturned: number[] = [...this.distances];
+    private distancesReturnedMap: Map<number, number> = new Map<number, number>();
+
+    public getNext(index: number): number {
+        console.log("getNext, index=", index);
+        assert(index >= 0, "index < 0");
+
+        if (this.distancesReturnedMap.has(index)) {
+            // value for this index has already been returned earlier
+            return this.distancesReturnedMap.get(index);
+        }
+
+        if (this.distancesNotYetReturned.length === 0) {
+            // all distances values have been randomly picked one time -> restart with all distances values
+            this.distancesNotYetReturned = [...this.distances];
+        }
+
+        // pick and remove one element of distancesNotYetReturned
+        console.log("this.distancesNotYetReturned before", this.distancesNotYetReturned);
+        const notYetReturnedIndex: number = createRandomNumber(0, this.distancesNotYetReturned.length - 1);
+        const next: number[] = this.distancesNotYetReturned.splice(notYetReturnedIndex, 1);
+        console.log("this.distancesNotYetReturned after", this.distancesNotYetReturned);
+        assert(next.length === 1, "next.length !== 1");
+
+        // remember picked element
+        this.distancesReturnedMap.set(index, next[0]);
+
+        return next[0];
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import {app, BrowserWindow} from 'electron';
-import {appManager} from "./electron/AppManager";
-import {TrayMenu} from "./electron/TrayMenu";
+import electronIsDev from "electron-is-dev";
 
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
@@ -10,6 +9,12 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 }
 
 const createWindow = (): void => {
+    if (electronIsDev) {
+        console.log('Running in development');
+    } else {
+        console.log('Running in production');
+    }
+
     // Create the browser window.
     const mainWindow = new BrowserWindow({
         fullscreenable: true,
@@ -27,7 +32,9 @@ const createWindow = (): void => {
     mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    if (electronIsDev) {
+        mainWindow.webContents.openDevTools();
+    }
 };
 
 // This method will be called when Electron has finished
@@ -37,7 +44,7 @@ const createWindow = (): void => {
 //     appManager.setTray(new TrayMenu());
 //     appManager.setWindow('MainWindow', createWindow);
 // });
-app.on('ready', ():void => {
+app.on('ready', (): void => {
     return createWindow();
 });
 
@@ -50,13 +57,10 @@ app.on('window-all-closed', (): void => {
     }
 });
 
-// app.on('activate', () => {
-//     // On OS X it's common to re-create a window in the app when the
-//     // dock icon is clicked and there are no other windows open.
-//     if (BrowserWindow.getAllWindows().length === 0) {
-//         createWindow();
-//     }
-// });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
