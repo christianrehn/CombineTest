557c4f16ae105544e91f9472280089c2ea952e14
rehn <rehn@itestra.de>
2021-12-09 14:47:45 +0100

    rename

--- src/app/components/NextDistanceBox/NextDistanceBox.tsx
+++ src/app/components/NextDistanceBox/NextDistanceBox.tsx
@@ -2,29 +2,29 @@ import React from "react";
 import './NextDistanceBox.scss';
 import {assert} from "chai";
 import {Unit} from "mathjs";
-import {ITestConfiguration} from "../../model/TestConfiguration";
+import {IDrillConfiguration} from "../../model/DrillConfiguration";
 
 export interface INextDistanceBoxProps {
     nextDistance: Unit;
-    selectedTestConfiguration: ITestConfiguration;
+    selectedDrillConfiguration: IDrillConfiguration;
 }
 
 export const NextDistanceBox: React.FC<INextDistanceBoxProps> = (props: INextDistanceBoxProps): JSX.Element => {
     assert(!!props, "!props");
-    assert(!!props.selectedTestConfiguration, "!props.selectedDistancesGenerator");
+    assert(!!props.selectedDrillConfiguration, "!props.selectedDistancesGenerator");
 
     const nextDistanceInDistancesGeneratorUnit: number =
         !!props.nextDistance
-            ? props.nextDistance.toNumber(props.selectedTestConfiguration.unit)
+            ? props.nextDistance.toNumber(props.selectedDrillConfiguration.unit)
             : undefined;
 
-    const averageStrokesFromStartDistance: number = props.selectedTestConfiguration.computeAverageStrokesFromStartDistance(props.nextDistance);
+    const averageStrokesFromStartDistance: number = props.selectedDrillConfiguration.computeAverageStrokesFromStartDistance(props.nextDistance);
 
     return (
         <div className="next-distance box">
             <p className="next-distance-number">{!!nextDistanceInDistancesGeneratorUnit ? nextDistanceInDistancesGeneratorUnit :
                 <span>&nbsp;</span>}</p>
-            <p className="next-distance-unit"> {!!nextDistanceInDistancesGeneratorUnit ? props.selectedTestConfiguration.unit : "DONE"}</p>
+            <p className="next-distance-unit"> {!!nextDistanceInDistancesGeneratorUnit ? props.selectedDrillConfiguration.unit : "DONE"}</p>
             <p className="next-distance-average-strokes">{!!averageStrokesFromStartDistance ? averageStrokesFromStartDistance.toFixed(3) :
                 <span>&nbsp;</span>}</p>
             <p className="next-distance-average-strokes-label">{!!averageStrokesFromStartDistance ? "Strokes" :
