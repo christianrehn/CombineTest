79b6b884fce1d98da295aa330faaa0b07c407c51
rehn <rehn@itestra.de>
2021-12-11 14:27:08 +0100

    rename

--- src/app/components/LastShotData/LastShotData.tsx
+++ src/app/components/LastShotData/LastShotData.tsx
@@ -4,7 +4,7 @@ import './LastShotData.scss';
 import * as math from "mathjs";
 import {Unit} from "mathjs";
 import {assert} from "chai";
-import {IDrillConfiguration} from "../../model/DrillConfiguration";
+import {IDrillConfiguration} from "../../model/drillconfiguration/DrillConfiguration";
 
 const SHOW_ADDITIONAL_DATA_FOR_ALL_SHOTS: boolean = false;
 
--- src/app/components/NextDistanceBox/NextDistanceBox.tsx
+++ src/app/components/NextDistanceBox/NextDistanceBox.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import './NextDistanceBox.scss';
 import {assert} from "chai";
 import {Unit} from "mathjs";
-import {IDrillConfiguration} from "../../model/DrillConfiguration";
+import {IDrillConfiguration} from "../../model/drillconfiguration/DrillConfiguration";
 
 export interface INextDistanceBoxProps {
     nextDistance: Unit;
