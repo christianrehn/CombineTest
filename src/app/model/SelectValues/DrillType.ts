fe41796b6519eb5d57ee1156d0c2ceb390110e8f
rehn <rehn@itestra.de>
2022-08-29 16:04:15 +0200

    new drill type: spin

---
+++ src/app/model/SelectValues/DrillType.ts
@@ -0,0 +1,3 @@
+export const shotsGainedDrillType: string = "ShotsGained";
+export const spinDrillType: string = "Spin";
+export const drillTypes: string[] = [shotsGainedDrillType, spinDrillType];
--- src/app/model/Unit/Unit.ts
+++
@@ -1,3 +0,0 @@
-export const meter: string = "meter";
-export const yards: string = "yards";
-export const lengthUnits: string[] = [meter, yards];
