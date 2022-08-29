fad89b4ded1063b85244afd2fe70747f208d890b
rehn <rehn@itestra.de>
2022-08-29 16:06:25 +0200

    new drill type: spin

--- src/app/components/DrillConfiguration/DrillConfigurationTextInput/DrillConfigurationTextInput.tsx
+++ src/app/components/DrillConfiguration/DrillConfigurationTextInput/DrillConfigurationTextInput.tsx
@@ -10,37 +10,38 @@ export interface IDrillConfigurationTextInputProps {
     min?: number;
     max?: number;
     error?: boolean;
+    visible?: boolean;
     handleOnChange: (value: string) => void;
 }
 
 export const DrillConfigurationTextInput: React.FC<IDrillConfigurationTextInputProps> = (props: IDrillConfigurationTextInputProps): any => {
-    return (
-        <div className="drill-configuration-text-input-container">
-            <label
-                className="drill-configuration-text-input-label"
-                htmlFor="drill-configuration-text-input">{props.label}
-            </label>
-            <div className="drill-configuration-text-input-box">
-                <input
-                    className={props.error ? "error drill-configuration-text-input input-css" : "drill-configuration-text-input input-css"}
-                    type={props.type}
-                    value={props.value}
-                    minLength={props.minLength}
-                    maxLength={props.maxLength}
-                    min={props.min}
-                    max={props.max}
-                    onChange={(event: React.ChangeEvent<HTMLInputElement>): void => {
-                        const value: string = String(event.target.value);
-                        if ((!!props.maxLength && value.length > props.maxLength)) {
-                            event.preventDefault();
-                            event.stopPropagation();
-                        } else {
-                            props.handleOnChange(String(event.target.value));
-                        }
-                    }}
-                />
-            </div>
-        </div>
+    return (props.visible ?
+            <div className="drill-configuration-text-input-container">
+                <label
+                    className="drill-configuration-text-input-label"
+                    htmlFor="drill-configuration-text-input">{props.label}
+                </label>
+                <div className="drill-configuration-text-input-box">
+                    <input
+                        className={props.error ? "error drill-configuration-text-input input-css" : "drill-configuration-text-input input-css"}
+                        type={props.type}
+                        value={props.value}
+                        minLength={props.minLength}
+                        maxLength={props.maxLength}
+                        min={props.min}
+                        max={props.max}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>): void => {
+                            const value: string = String(event.target.value);
+                            if ((!!props.maxLength && value.length > props.maxLength)) {
+                                event.preventDefault();
+                                event.stopPropagation();
+                            } else {
+                                props.handleOnChange(String(event.target.value));
+                            }
+                        }}
+                    />
+                </div>
+            </div> : null
     );
 
 }
