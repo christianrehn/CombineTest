bd6c0fc5444f543aa994ae66c46179abb46cf387
rehn <rehn@itestra.de>
2022-09-13 21:35:00 +0200

    maxDeviationAsUnitNotPercent

--- src/app/components/DrillConfiguration/NumberPlusMinusInput/NumberPlusMinusInput.tsx
+++ src/app/components/DrillConfiguration/NumberPlusMinusInput/NumberPlusMinusInput.tsx
@@ -6,16 +6,17 @@ export interface INumberOfShotsInputProps {
     value: number;
     min?: number;
     max?: number;
-    handleOnClick: (numberOfShots: number) => void;
+    hidden?: boolean;
+    handleOnClick: (value: number) => void;
 }
 
 export const NumberPlusMinusInput: React.FC<INumberOfShotsInputProps> = (props: INumberOfShotsInputProps): any => {
-    return (
-        <div className="number-of-shots-input-container">
-            <label
-                className="number-of-shots-input-label"
-                htmlFor="number-of-shots-input">{props.label}</label>
-            <div className="btn-change-box">
+    return (props.hidden ? null :
+            <div className="number-of-shots-input-container">
+                <label
+                    className="number-of-shots-input-label"
+                    htmlFor="number-of-shots-input">{props.label}</label>
+                <div className="btn-change-box">
                 <span className="btn-change minus icon icon-minus"
                       onClick={(): void => {
                           if (!props.min || props.value > props.min) {
@@ -24,23 +25,23 @@ export const NumberPlusMinusInput: React.FC<INumberOfShotsInputProps> = (props: 
                       }}>
                     -
                 </span>
-                <input
-                    className="number-of-shots-input input-css"
-                    type="text"
-                    value={props.value}
-                    min="1"
-                    readOnly={true}
-                />
-                <span className="btn-change plus icon icon-plus"
-                      onClick={(): void => {
-                          if (!props.max || props.value < props.max) {
-                              props.handleOnClick(props.value + 1);
-                          }
-                      }}>
+                    <input
+                        className="number-of-shots-input input-css"
+                        type="text"
+                        value={props.value}
+                        min="1"
+                        readOnly={true}
+                    />
+                    <span className="btn-change plus icon icon-plus"
+                          onClick={(): void => {
+                              if (!props.max || props.value < props.max) {
+                                  props.handleOnClick(props.value + 1);
+                              }
+                          }}>
                     +
                 </span>
+                </div>
             </div>
-        </div>
     );
 
 }
