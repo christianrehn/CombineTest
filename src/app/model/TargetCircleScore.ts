50cfab92fc2360a78bd9766321dc7cae8f873626
rehn <rehn@itestra.de>
2022-09-15 12:38:59 +0200

    target circle score computation

--- src/app/model/TargetCircleScore.ts
+++ src/app/model/TargetCircleScore.ts
@@ -3,7 +3,11 @@ import {assert} from "chai";
 import {IDrillConfiguration} from "./DrillConfiguration/DrillConfiguration";
 import {targetCircleDrillType} from "./SelectValues/DrillType";
 
-
+/**
+ * If fromPin equals the radius of the inner circle score is 100.
+ * Score is linear decreased to zero to the radius of the outer circle.
+ * Score is linear increased by the same amount up to the targetDistance.
+ */
 export const computeTargetCircleScore = (drillConfiguration: IDrillConfiguration, targetDistance: Unit, fromPin: Unit): number => {
     assert(!!drillConfiguration, "!drillConfiguration");
     assert(!!targetDistance, "!targetDistance");
@@ -22,11 +26,24 @@ export const computeTargetCircleScore = (drillConfiguration: IDrillConfiguration
     const targetDistanceInUnitAsNumber: number = targetDistance.toNumber(drillConfiguration.getUnit());
     const fromPinInUnitAsNumber: number = fromPin.toNumber(drillConfiguration.getUnit());
 
-    const maxFromPinInUnitAsNumber: number =
+    const targetCircleRadiusScore100InUnitAsNumber: number =
         drillConfiguration.getTargetCircleRadiusAsUnitNotPercent()
             ? drillConfiguration.getTargetCircleRadiusScore100InUnit()
             : drillConfiguration.getTargetCircleRadiusScore100InPercent() * targetDistanceInUnitAsNumber / 100;
-    console.log("maxFromPinInUnitAsNumber", maxFromPinInUnitAsNumber)
+    console.log("targetCircleRadiusScore100InUnitAsNumber", targetCircleRadiusScore100InUnitAsNumber);
+    const targetCircleRadiusScore0InUnitAsNumber: number =
+        drillConfiguration.getTargetCircleRadiusAsUnitNotPercent()
+            ? drillConfiguration.getTargetCircleRadiusScore0InUnit()
+            : drillConfiguration.getTargetCircleRadiusScore0InPercent() * targetDistanceInUnitAsNumber / 100;
+    console.log("targetCircleRadiusScore0InUnitAsNumber", targetCircleRadiusScore0InUnitAsNumber);
 
-    return Math.max(Math.round((maxFromPinInUnitAsNumber + (maxFromPinInUnitAsNumber - fromPinInUnitAsNumber)) * 1000 / maxFromPinInUnitAsNumber) / 10, 0);
+    const targetCircleRadiusDeltaInUnitAsNumber: number = targetCircleRadiusScore0InUnitAsNumber - targetCircleRadiusScore100InUnitAsNumber;
+    console.log("targetCircleRadiusDeltaInUnitAsNumber", targetCircleRadiusDeltaInUnitAsNumber);
+
+    // fromPinDeltaInUnitAsNumber is negative outside inner circle
+    const fromPinDeltaInUnitAsNumber: number = fromPinInUnitAsNumber - targetCircleRadiusScore100InUnitAsNumber;
+    console.log("fromPinDeltaInUnitAsNumber", fromPinDeltaInUnitAsNumber);
+
+    // (delta_circle - delta_fromPin) * 100 / delta_circle
+    return Math.round((targetCircleRadiusDeltaInUnitAsNumber - fromPinDeltaInUnitAsNumber) * 1000 / targetCircleRadiusDeltaInUnitAsNumber) / 10;
 }
