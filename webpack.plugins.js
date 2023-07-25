1a18e7060ab6b6d119f77eda2e84f1b234630485
rehn <rehn@itestra.de>
2023-07-25 18:38:54 +0200

    fsevents only under macos

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,5 +1,13 @@
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
+const {IgnorePlugin} = require('webpack');
+
+const optionalPlugins = [];
+if (process.platform !== "darwin") { // don't ignore on OSX
+    optionalPlugins.push(new IgnorePlugin({resourceRegExp: /^fsevents$/}));
+}
+
 module.exports = [
-  new ForkTsCheckerWebpackPlugin()
+    new ForkTsCheckerWebpackPlugin(),
+    ...optionalPlugins,
 ];
